2c7c7ce52f0a08c5557a4099c0fe8942a683d210
LautaroCruz1 <snochiasd@gmail.com>
2023-09-19 17:55:25 -0300

    clase 3 varios ejemplos de funciones

---
+++ funciones.js
@@ -0,0 +1,72 @@
+// D.R.Y , K-I-S-S, Y-A-G-N-I.
+// Dont repeat yourself, keep it simple stupid, if you are not going to need it, delete it
+
+// function product (){
+//     let a = 2;
+//     let b = 4;
+//     return a * b
+// }
+
+// console.log(product());
+
+
+// Funciones declaradas:
+// function retornarString (){
+//     console.log("uno");
+//     console.log("dos");
+//     return "la funcion devuelve un string de texto";
+// }
+
+// let devolverValor = retornarString()
+
+// console.log(devolverValor);
+
+
+// function saludar (nombre, edad) {
+//     console.log("Hola mi nombre es " + nombre + " y tengo " + edad + " años");
+// }
+
+// saludar("Lautaro" , 24);
+
+// Funciones expresadas:
+// const funcionExpresada = function(){
+//     console.log("Hola soy una función expresada");
+// }
+
+// funcionExpresada()
+
+
+// let resultado = 0;
+
+// function sumar(numeroA , numeroB){
+//     resultado = numeroA + numeroB
+// }
+
+// function mostrar (mensaje){
+//     console.log(mensaje);
+// }
+
+// sumar(6,3)
+
+// mostrar(resultado)
+
+
+// let auto = "descapotable"
+
+// function pasear (){
+//     let otroAuto = "mercedes"
+//     function lavar() {
+//         console.log("enviar a lavar el " + otroAuto);
+//     }
+//     lavar()
+//     console.log("paseando en el " + auto);
+// }
+
+// pasear()
+
+const multiplicarXdos = function(numero){
+    console.log(numero*2)
+}
+
+multiplicarXdos(10);
+
